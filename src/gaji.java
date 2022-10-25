f4380de91060e143dffe780f937055c6f2c478ab
Kiki Haerani <101385741+kikihaerani@users.noreply.github.com>
2022-10-25 11:35:55 +0800

    Add files via upload

---
+++ src/gaji.java
@@ -0,0 +1,14 @@
+public class gaji {
+    Integer gajiPokok;
+    Integer gajiTunjanganJabatan;
+    Integer gajiLembur;
+
+
+    public gaji(Integer gajiPokok, Integer gajiTunjanganJabatan, Integer gajiLembur){
+        this.gajiPokok=gajiPokok;
+        this.gajiTunjanganJabatan=gajiTunjanganJabatan;
+        this.gajiLembur=gajiLembur;
+
+
+    }
+}
